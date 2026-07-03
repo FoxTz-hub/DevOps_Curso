7c17037faaeda06d04f17ddb741073ad388e99e9
Luiz Ferlin <55118601+FoxTz-hub@users.noreply.github.com>
2026-07-03 16:30:24 +0000

    Criacao primeiro endpoint

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 from fastapi import FastAPI
+from datetime import  datetime
 
+LISTA_TAREFAS = []
 APP = FastAPI()
+
+def nova_tarefa(id: int, titulo: str, descricao: str):
+    return{
+        "id": id,
+        "titulo": titulo,
+        "descricao": descricao,
+        "concluido": False,
+        "criado_em": datetime.now()
+    }
 
 @APP.get("/")
 def index():
     return "Olá, DevOps!"
+
+@APP.get("/terefas")
+def listar_tarefas():
+    #Listar Tarefas (somente com id e titulo)
+    if len(LISTA_TAREFAS) == 0:
+        return LISTA_TAREFAS
+
+    tarefa = []
+
+    for tarefa in LISTA_TAREFAS:
+        info = {"id": tarefa['id'], "titulo": tarefa['titulo']}
+        tarefa.append(info)
+    
+    return tarefa
